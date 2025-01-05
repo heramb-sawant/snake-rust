62c06e6033ed6533f6721d03a1de96bab72c7d20
Heramb Sawant <herambsawant11@gmail.com>
2025-01-05 13:12:57 -0700

    Grid printing, get input logs working

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,15 @@
 use rand::{thread_rng, Rng};
-use std::fmt::{Display, Formatter, Result};
+use termion::event::Key;
+use termion::input::{Keys, TermRead};
+use termion::raw::{IntoRawMode, RawTerminal};
+use termion::{clear, color, cursor, style};
+
+use std::io::{stdin, stdout, Stdin, Stdout, Write};
 
 /*
     TODO:
         - Randomly generate monsters on the board
-        - Get display using termion or crossterm
+        - Get display using termion
         - Get movements working
         - A few tests
 
@@ -36,10 +41,12 @@ struct Grid {
     tiles: Vec<Vec<Tile>>,
     columns: usize,
     rows: usize,
+    stdout: RawTerminal<Stdout>,
+    stdin: Keys<Stdin>,
 }
 
 impl Grid {
-    fn new(columns: usize, rows: usize) -> Grid {
+    fn new(columns: usize, rows: usize, stdout: RawTerminal<Stdout>, stdin: Stdin) -> Grid {
         let mut empty_grid = vec![vec![Tile::Empty; rows]; columns];
 
         // Add walls
@@ -61,8 +68,8 @@ impl Grid {
         empty_grid[1][1] = Tile::Player;
         empty_grid[columns - 2][rows - 2] = Tile::Exit;
 
-        let enemy_column: usize = thread_rng().gen_range(2..19);
-        let enemy_row: usize = thread_rng().gen_range(2..19);
+        let enemy_column: usize = thread_rng().gen_range(2..18);
+        let enemy_row: usize = thread_rng().gen_range(2..18);
 
         empty_grid[enemy_column][enemy_row] = Tile::Enemy;
 
@@ -70,15 +77,66 @@ impl Grid {
             tiles: empty_grid,
             columns,
             rows,
+            stdout,
+            stdin: stdin.keys(),
         };
     }
 
-    fn print(&self) {
-        for columns in self.tiles.iter().rev() {
-            for row in columns.iter() {
-                print!("{}", row.get_character())
+    fn start(&mut self) {
+        // TODO: This clears some of the warns I think. Make sure it starts after all the console start output
+        write!(self.stdout, "{}", clear::All).unwrap();
+        // self.stdout.flush().unwrap();
+
+        self.print();
+
+        write!(
+            self.stdout,
+            "{}Arrow keys to move, q to exit.{}",
+            termion::cursor::Goto(0, 21),
+            termion::cursor::Hide
+        )
+        .unwrap();
+
+        loop {
+            let c = self.stdin.next().unwrap().unwrap();
+            // Clear the current line.
+            write!(
+                self.stdout,
+                "{}{}",
+                termion::cursor::Goto(0, 22),
+                termion::clear::CurrentLine
+            )
+            .unwrap();
+
+            match c {
+                // Exit.
+                Key::Char('q') => break,
+                Key::Left => println!("<left>"),
+                Key::Right => println!("<right>"),
+                Key::Up => println!("<up>"),
+                Key::Down => println!("<down>"),
+                _ => println!("Invalid Move"),
             }
-            println!("")
+
+            // Flush again.
+            self.stdout.flush().unwrap();
+        }
+
+        // Show the cursor again before we exit.
+        write!(self.stdout, "{}", termion::cursor::Show).unwrap();
+    }
+
+    // TODO: get rid of this mut with pointers or whatever you saw in the tutorial.
+    fn print(&mut self) {
+        // TODO: Update all the tiles to have a coordinate so its easier to reference and print them at a specific position
+        let mut i = 1;
+        for columns in self.tiles.iter().rev() {
+            write!(self.stdout, "{}", termion::cursor::Goto(0, i)).unwrap();
+            i += 1;
+            for row in columns.iter() {
+                write!(self.stdout, "{}", row.get_character()).unwrap()
+            }
+            writeln!(self.stdout, "").unwrap()
         }
     }
 }
@@ -86,8 +144,12 @@ impl Grid {
 fn main() {
     println!("Starting!");
 
-    let length = 20;
-    let grid = Grid::new(length, length);
+    let stdin = stdin();
+    let stdout = stdout().into_raw_mode().unwrap();
 
-    grid.print();
+    // TODO: Get grid size from input.
+    let length = 20;
+    let mut grid = Grid::new(length, length, stdout, stdin);
+
+    grid.start();
 }
